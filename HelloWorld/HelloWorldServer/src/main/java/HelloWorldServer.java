54109371676bd901d10249b3bdef410f9ba6b18e
CodeCoderCoding <1845371176@qq.com>
2022-07-12 16:55:50 +0800

    添加HelloWorld项目

---
+++ HelloWorld/HelloWorldServer/src/main/java/HelloWorldServer.java
@@ -0,0 +1,29 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @ClassName HelloWorldServer
+ * @Description
+ * @Date 2022/7/12 16:42
+ * @Version 1.0.0
+ **/
+public class HelloWorldServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                    }
+                });
+
+        serverBootstrap.bind(8083);
+    }
+}
